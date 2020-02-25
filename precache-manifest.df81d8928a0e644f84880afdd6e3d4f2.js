a750c6448656f151723be38265bbe6c4e7f5dae8
Ilyin Sergei <steelseriesisv@gmail.com>
2020-02-25 18:08:34 +0200

    Updates

---
+++ precache-manifest.df81d8928a0e644f84880afdd6e3d4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8c37dbf76be553024013863426d443",
+    "url": "/tomnium-test-task/index.html"
+  },
+  {
+    "revision": "2d8524eae4de48a13709",
+    "url": "/tomnium-test-task/static/css/main.9ae3c21f.chunk.css"
+  },
+  {
+    "revision": "f1daf381733c5885238b",
+    "url": "/tomnium-test-task/static/js/2.6de79882.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/tomnium-test-task/static/js/2.6de79882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8524eae4de48a13709",
+    "url": "/tomnium-test-task/static/js/main.b6b9f6b6.chunk.js"
+  },
+  {
+    "revision": "5627067b78372833fe17",
+    "url": "/tomnium-test-task/static/js/runtime-main.b8c1b81f.js"
+  }
+]);
